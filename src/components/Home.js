d71248ba4dcafdbaa7ffb2178d222f2d4e84dd40
erkin312 <erkin.kk@gmail.com>
2022-01-20 22:35:04 +0600

    Popular movies page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import { useHomeFetch } from '../hooks/useHomeFetch';
+import Grid from './Grid/Grid';
 import HeroImage from './HeroImage/HeroImage';
+import Thumb from './Thumb/Thumb';
+
+import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
@@ -15,6 +19,22 @@ const Home = () => {
           text={state.results[0].overview}
         />
       )}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => {
+          return (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+              }
+              movieId={movie.id}
+            />
+          );
+        })}
+      </Grid>
     </>
   );
 };
